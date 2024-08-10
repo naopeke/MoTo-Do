e942294025e696570919eb04c31c8ffd2c7dc231
naopeke <naopeke@protonmail.com>
2024-08-10 20:00:59 +0200

    back

---
+++ src/app/lib/placeholder-data.ts
@@ -0,0 +1,14 @@
+export const todos = [
+    {
+        description: 'postgres',
+        isDone: 'true'
+    },
+    {
+        description: 'postgres advanced',
+        isDone: 'false'
+    },
+    {
+        description: 'typescript',
+        isDone: 'false'
+    },
+]
