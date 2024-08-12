a0b993d7a5ba9b8bb7ffb74834a7e8479ff40d02
naopeke <naopeke@protonmail.com>
2024-08-12 18:25:46 +0200

    login failed

--- auth.config.ts
+++ auth.config.ts
@@ -1,24 +1,24 @@
-import type { NextAuthConfig } from 'next-auth';
+// import type { NextAuthConfig } from 'next-auth';
  
-export const authConfig = {
-  pages: {
-    signIn: '/login',
-  },
-  callbacks: {
-    authorized({ auth, request: { nextUrl } }) {
-      const isLoggedIn = !!auth?.user;
-      const isOnTodo = nextUrl.pathname.startsWith('/todo');
-      if (isOnTodo) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL('/todo', nextUrl));
-      }
-      return true;
-    },
-  },
-  providers: [], // Add providers with an empty array for now
-} satisfies NextAuthConfig;
+// export const authConfig = {
+//   pages: {
+//     signIn: '/login',
+//   },
+//   callbacks: {
+//     authorized({ auth, request: { nextUrl } }) {
+//       const isLoggedIn = !!auth?.user;
+//       const isOnTodo = nextUrl.pathname.startsWith('/todo');
+//       if (isOnTodo) {
+//         if (isLoggedIn) return true;
+//         return false; // Redirect unauthenticated users to login page
+//       } else if (isLoggedIn) {
+//         return Response.redirect(new URL('/todo', nextUrl));
+//       }
+//       return true;
+//     },
+//   },
+//   providers: [], // Add providers with an empty array for now
+// } satisfies NextAuthConfig;
 
 //The authorized callback is used to verify if the request is authorized to access a page via Next.js Middleware. It is called before a request is completed, and it receives an object with the auth and request properties. The auth property contains the user's session, and the request property contains the incoming request.
 //The providers option is an array where you list different login options. 
