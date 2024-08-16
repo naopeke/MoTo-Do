766f44f7046b123322879605e08d21a6b27878a0
naopeke <naopeke@protonmail.com>
2024-08-16 16:49:13 +0200

    todocollection

--- src/app/todo/[...id]/page.tsx
+++ src/app/todo/[...id]/page.tsx
@@ -24,12 +24,24 @@ export default function TodoPage() {
       }
     }
     fetchCollectionName(); 
-  }, [collection_id])
+  }, [collection_id]);
+
+  const handleBack = () => {
+    router.back();
+  };
 
   return (
-    <div className={`flex min-h-screen flex-col items-center p-24`}>
+    <>
+    <div className="relative flex min-h-screen flex-col items-center p-24">
+      <div className="absolute top-6 right-20">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 cursor-pointer" onClick={handleBack}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
+        </svg>
+      </div>
+
       <h1 className="m-3 font-extrabold">{collectionName}</h1>
        <TodoList></TodoList>
     </div>
+    </>
   );
 }
